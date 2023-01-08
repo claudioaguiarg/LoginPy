d979e4d861ff1400dd93b36329c6f5b7000e39a2
Cláudio Aguiar <claudioaguiargomes@gmail.com>
2023-01-08 12:27:02 -0400

    add

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import sqlite3
 from tkinter import *
 import customtkinter
-from tkinter import ttk
-from PIL import Image, ImageTk
+from PIL import Image
 
 con = sqlite3.connect("base.db")
 cur = con.cursor()
@@ -18,12 +17,15 @@ class SistemaLogin():
         self.main.geometry("400x240")
         self.main.resizable(width=False, height=False)
 
-        self.default()
+        self.feedback = customtkinter.CTkLabel(master=self.main, text='')
+        self.feedback.pack(side=BOTTOM)
 
+        self.default()
 
         self.main.mainloop()
 
     def default(self):
+        #Destroys widgets of others pages(register)
         self.destroy_items()
         #Username Frame
         self.username_tittle = customtkinter.CTkLabel(master=self.main, text='Username:')
@@ -54,14 +56,17 @@ class SistemaLogin():
                              self.enter_buttom,
                              self.signup_buttom]
     def register(self):
+        #Destroys the main widgets
         self.destroy_items()
         #Username box register
-        self.username_tittle = customtkinter.CTkLabel(master=self.main, text='Username:').place(relx=0.2, rely= 0.3)
+        self.username_tittle = customtkinter.CTkLabel(master=self.main, text='Username:')
+        self.username_tittle.place(relx=0.2, rely= 0.3)
         self.username_box_register = customtkinter.CTkEntry(master=self.main,width=150)
         self.username_box_register.place(relx=0.43, rely=0.3)
         
         #Password box reister
-        self.password_tittle = customtkinter.CTkLabel(self.main, text='Password:').place(relx=0.2, rely= 0.45)
+        self.password_tittle = customtkinter.CTkLabel(self.main, text='Password:')
+        self.password_tittle.place(relx=0.2, rely= 0.45)
         self.password_register_box = customtkinter.CTkEntry(self.main,width=150, show='*')
         self.password_register_box.place(relx=0.43, rely=0.45)
 
@@ -79,54 +84,80 @@ class SistemaLogin():
                              self.return_buttom,
                              self.signup_buttom]
         
-
+    #Define a function that registers username and password at DB
     def register_db(self):
+        #catch the username and password
         username = self.username_box_register.get()
         password = self.password_register_box.get()
-        try:
-            cur.execute(f"""
-                        INSERT INTO users VALUES
-                        ('{username}','{password}')
-                        """)
-            con.commit()
-            print('Sucess')
-        except:
-            print('ERROR [DATABASE REGISTER]')
+        #virify if username or password is empty
+        if len(username) == 0 or len(password) == 0:
+            self.feedback_message('The boxes cannot be empty')
+        else:
+            try:
+                #search in database if username already exists
+                res = cur.execute(f"SELECT * FROM users WHERE name='{username}'")
+                res = res.fetchall()
+                if len(res) != 0:
+                    self.feedback_message('This username already exist')
+                else:
+                    try:
+                        cur.execute(f"""
+                                    INSERT INTO users VALUES
+                                    ('{username}','{password}')
+                                    """)
+                        con.commit()
+                        print('Sucess')
+                    except:
+                        self.feedback_message('ERROR [DATABASE REGISTER]')
+            except:
+                self.feedback_message('ERROR [USER VALIDATION]')
         
-        
-
     #Define a function that verify the authorization
     def loginAuthorization(self):
         #verifica username na db
         try:
             username = self.username_box.get()
-            res = cur.execute(f"SELECT * FROM users WHERE name='{username}'")
-            res = res.fetchall()
         except:
-            print('Database connection error')
+            self.feedback_message('Username get-error')
 
         #verifica password na db
         try:
             password = self.password_box.get()
         except:
-            print('Password get-error')
+            self.feedback_message('Password get-error')
 
-        #se autorizado, deleta a main e inicia a pagina de autorização
-        try:
-            if password == res[0][1]:
-                print('Sucess')
+        if len(username) == 0 or len(password) == 0:
+            self.feedback_message('The boxes cannot be empty')
+        else:
+            res = cur.execute(f"SELECT * FROM users WHERE name='{username}'")
+            res = res.fetchall()
+            if len(res) == 0:
+                self.feedback_message('unregistered user')
             else:
-                print('Wrong username or password')
-        except:
-            print('Database error')
-        #senão mostra mensagem de erro
+                try:
+                    if password == res[0][1]:
+                        self.feedback_message('Sucess')
+                    else:
+                        self.feedback_message('Wrong username or password')
+                except:
+                    self.feedback_message('Database error')
+               
     
+   
     def destroy_items(self):
         try:
             for item in self.list_destroy:
                 item.destroy()
         except:
-            print('Error [Destroy Items]')
+            self.feedback_message('Error [Destroy Items]')
+
+    def feedback_message(self,message):
+        self.feedback.configure(text=message)
+        self.feedback.after(5000, self.clear_feedback)
+
+    def clear_feedback(self):
+        self.feedback.configure(text='')
+
 #Create a Ctk instance(app)
 main = SistemaLogin()
 main()
